efad64c7da91486444962c678f979a54e61aeda7
StinglSa <s0521204@htw-berlin.de>
2015-05-30 00:32:11 +0200

    wrong urls

--- app/urls.py
+++ app/urls.py
@@ -4,10 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-                       (r'^polls/$', 'polls.views.index'),
-                       (r'^polls/(?P<poll_id>\d+)/$', 'polls.views.details'),
-                       (r'^polls/(?P<poll_id>\d+)/results/$', 'polls.views.results'),
-                       (r'^polls/(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
 
     # Examples:
     # url(r'^$', 'app.views.home', name='home'),
